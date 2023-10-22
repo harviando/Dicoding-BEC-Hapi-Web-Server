4f1006b456b51ab0fb4492c53c5829462ad5ecae
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-22 21:41:02 +0700

    feat: Move server route handling on routes.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log("I will be cloud architect that work remotely!");
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
 
@@ -10,27 +11,13 @@ const init = async () => {
         host: 'localhost',
     });
 
+    //Server route
+    server.route(routes);
+
     // Server start
     await server.start();
     console.log(`Server is running on ${server.info.uri}`);
 
-    //Server route
-    server.route([
-        {
-            method: 'GET',
-            path: '/',
-            handler: (request, h) => {
-                return 'Home page.';
-            }
-        },
-        {
-            method: 'GET',
-            path: '/about',
-            handler: (request, h) => {
-                return 'About page.';
-            }
-        },
-    ]);
 }
 
 init();
