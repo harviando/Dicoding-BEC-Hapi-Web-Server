2db32f30d3f59bab9f8458b3a1feee0792f9e408
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-22 21:24:47 +0700

    feat: Import hapi module into server.js and add server init

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("I will be cloud architect that work remotely!");
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5001,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server is running on ${server.info.uri}`);
+}
+
+init();
