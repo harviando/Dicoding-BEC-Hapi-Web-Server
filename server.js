04ea82e03cabde04f49a6cdbdea65d2da965db61
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-22 21:37:33 +0700

    feat: Add server route for home and about page

--- server.js
+++ server.js
@@ -3,13 +3,34 @@ console.log("I will be cloud architect that work remotely!");
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+
+    // Server init
     const server = Hapi.server({
         port: 5001,
         host: 'localhost',
     });
 
+    // Server start
     await server.start();
     console.log(`Server is running on ${server.info.uri}`);
+
+    //Server route
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return 'Home page.';
+            }
+        },
+        {
+            method: 'GET',
+            path: '/about',
+            handler: (request, h) => {
+                return 'About page.';
+            }
+        },
+    ]);
 }
 
 init();
